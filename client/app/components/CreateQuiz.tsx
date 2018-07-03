4409474b3d79d0431dda1ef4072ede8f13683d5c
David S <davidserna2005@gmail.com>
2018-07-03 13:47:53 -0500

    create quiz component

---
+++ client/app/components/CreateQuiz.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import fontawesome from '@fortawesome/fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export class CreateQuizComponent extends Component {
+  render() {
+    return (
+      <div>
+        <Link to="/add-quiz" className="create-quiz-page">
+          Add Quiz
+        </Link>
+        <Link to="/test-quiz" className="create-quiz-page">
+          Test Quiz
+        </Link>
+        <Link to="/view-quiz" className="create-quiz-page">
+          View Quizzes
+        </Link>
+      </div>
+    )
+  }
+}
+
+// const mapStateToProps = state => ({
+//   username: state.auth.username,
+//   email: state.auth.email,
+//   firstname: state.auth.firstname
+// })
+
+// export default connect(mapStateToProps)(CreateQuizComponent)
