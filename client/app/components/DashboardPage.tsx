649ae660da61225434636881777b5a9993a9604d
Fernando Hudson <fernando_hudson@redlands.edu>
2018-07-03 14:45:01 -0400

    links on dashboard are created.

--- client/app/components/DashboardPage.tsx
+++ client/app/components/DashboardPage.tsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import moment from 'moment'
 import numeral from 'numeral'
+import { Link } from 'react-router-dom'
 
 export const DashboardPage = props => (
   <div className="dashboard-page">
     <h1>dashboard</h1>
+    <Link to="/createQuiz" className="link">
+      Create A Quiz
+    </Link>
+    <Link to="/viewQuizzes" className="link">
+      Take A Quiz
+    </Link>
   </div>
 )
 
