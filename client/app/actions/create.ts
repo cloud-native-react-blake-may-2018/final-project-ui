89f60bb65df871fb16e006ea64b18ee372b3cb0b
RGWantz <ruthanna.wantz17@houghton.edu>
2018-07-03 14:41:17 -0400

    create functions added

---
+++ client/app/actions/create.ts
@@ -0,0 +1,46 @@
+import React from "react";
+import api from "../path-list";
+
+// export const persist = identity => ({
+//   type: "PERSIST",
+//   identity
+// });
+
+// export const startPersist = identity => {
+//   return dispatch => {
+//     dispatch(persist(identity))
+
+//     return api.user.persistUser(identity).then(data => {
+//       const user = {
+//         ...data,
+//         token: identity.token
+//       }
+//       return dispatch(login(user))
+//     })
+//   }
+// }
+
+export const createNewQuestion = newQuestion => ({
+  type: "CREATE_NEW_QUESTION",
+  newQuestion
+});
+
+export const startcreateNewQuestion = newQuestion => dispatch => {
+  dispatch(createNewQuestion(newQuestion));
+};
+
+export const createNewQuiz = newQuiz => ({
+  type: "CREATE_NEW_QUIZ",
+  newQuiz
+});
+
+export const startcreateNewQuiz = newQuiz => dispatch => {
+  dispatch(createNewQuiz(newQuiz));
+};
+
+// export const updateUser = user => ({
+//   type: "UPDATE_USER",
+//   user
+// });
+
+// export const startUpdateUser = user => dispatch => dispatch(updateUser(user));
