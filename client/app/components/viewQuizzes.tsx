dfa6d07d910a0c5be87b9d3d062c1f6e59b8112f
Fernando Hudson <fernando_hudson@redlands.edu>
2018-07-05 16:23:52 -0400

    displayQuizzes

--- client/app/components/viewQuizzes.tsx
+++ client/app/components/viewQuizzes.tsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import moment from 'moment'
 import numeral from 'numeral'
+import { connect } from 'http2'
 
-export const ViewQuizzesPage = props => (
-  <div className="viewQuizzes-page">
-    <h1>View Quizzes</h1>
-  </div>
-)
+interface IProps {}
 
-export default ViewQuizzesPage
+export class ViewQuizzesPage extends Component<IProps> {
+  render() {
+    return (
+      <div className="viewQuizzes-page">
+        <h1>View Quizzes</h1>
+        {this.props.quizzes.map}
+      </div>
+    )
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { viewQuizzes }
+)(ViewQuizzesPage)
--- client/app/path-list.ts
+++ client/app/path-list.ts
@@ -1,16 +1,19 @@
-import axios from "axios";
+import axios from 'axios'
 
-const authAxios = axios.create();
+const authAxios = axios.create()
 authAxios.interceptors.request.use(config => {
-  config.headers.Authorization = localStorage.token;
-  return config;
-});
+  config.headers.Authorization = localStorage.token
+  return config
+})
 
 const addQuestionUrl =
-  "https://eyc3l7k6w1.execute-api.us-east-2.amazonaws.com/dev/question";
+  'https://eyc3l7k6w1.execute-api.us-east-2.amazonaws.com/dev/question'
 
 const addQuizUrl =
-  "https://eyc3l7k6w1.execute-api.us-east-2.amazonaws.com/dev/quiz";
+  'https://eyc3l7k6w1.execute-api.us-east-2.amazonaws.com/dev/quiz'
+
+const displayQuizzes =
+  'https://eyc3l7k6w1.execute-api.us-east-2.amazonaws.com/dev/author/'
 
 export default {
   create: {
@@ -18,5 +21,9 @@ export default {
       axios.post(addQuestionUrl, { newQuestion }).then(res => res.data),
     addQuiz: newQuiz =>
       axios.post(addQuizUrl, { newQuiz }).then(res => res.data)
+  },
+  quiz: {
+    displayQuizzes: author =>
+      axios.get(displayQuizzes + author).then(res => res.data)
   }
-};
+}
