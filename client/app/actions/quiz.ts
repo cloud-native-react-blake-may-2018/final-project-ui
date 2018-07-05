6472171f0b6dfa9689f10329ab05ef17c46c8237
Eric Morrison <malin1@protonmail.ch>
2018-07-05 16:27:41 -0400

    fixed typo in quiz action creator

--- client/app/actions/quiz.ts
+++ client/app/actions/quiz.ts
@@ -2,7 +2,7 @@ import React from 'react'
 import api from '../path-list'
 
 export const startEditQuiz = quiz => dispatch =>
-  api.quiz.edit(quiz).then(quiz => dispatch(editQuiz(quiz)))
+  api.quizzes.edit(quiz).then(quiz => dispatch(editQuiz(quiz)))
 
 export const editQuiz = quiz => ({
   type: 'EDIT_QUIZ',
