8ff9a5d0e7279b66dd3c4cb629808698fab01717
Eric Morrison <malin1@protonmail.ch>
2018-07-05 16:23:16 -0400

    added quiz and question base for actions and reducers

---
+++ client/app/actions/questions.ts
@@ -0,0 +1,19 @@
+import React from 'react'
+import api from '../path-list'
+
+// export const createNewQuestion = newQuestion => ({
+//   type: 'CREATE_NEW_QUESTION',
+//   newQuestion
+// })
+
+// export const startCreateNewQuestion = newQuestion =>
+//   // dispatch(createNewQuestion(newQuestion))
+
+// export const createNewQuiz = newQuiz => ({
+//   type: 'CREATE_NEW_QUIZ',
+//   newQuiz
+// })
+
+// export const startCreateNewQuiz = newQuiz => dispatch => {
+//   dispatch(createNewQuiz(newQuiz))
+// }
--- client/app/path-list.ts
+++ client/app/path-list.ts
@@ -1,16 +1,22 @@
-import axios from "axios";
+import axios from 'axios'
 
-const authAxios = axios.create();
+const authAxios = axios.create()
 authAxios.interceptors.request.use(config => {
-  config.headers.Authorization = localStorage.token;
-  return config;
-});
+  config.headers.Authorization = localStorage.token
+  return config
+})
 
 const addQuestionUrl =
-  "https://eyc3l7k6w1.execute-api.us-east-2.amazonaws.com/dev/question";
+  'https://eyc3l7k6w1.execute-api.us-east-2.amazonaws.com/dev/question'
+
+const editQuestionUrl = ''
 
 const addQuizUrl =
-  "https://eyc3l7k6w1.execute-api.us-east-2.amazonaws.com/dev/quiz";
+  'https://eyc3l7k6w1.execute-api.us-east-2.amazonaws.com/dev/quiz'
+
+const editQuizUrl = ''
+
+const deleteQuizUrl = ''
 
 export default {
   create: {
@@ -18,5 +24,15 @@ export default {
       axios.post(addQuestionUrl, { newQuestion }).then(res => res.data),
     addQuiz: newQuiz =>
       axios.post(addQuizUrl, { newQuiz }).then(res => res.data)
+  },
+
+  quizzes: {
+    edit: quiz => axios.post(editQuizUrl, { quiz }).then(res => res.data),
+    delete: quiz => axios.post(deleteQuizUrl, { quiz }).then(res => res.data)
+  },
+
+  questions: {
+    edit: question =>
+      axios.post(editQuestionUrl, { question }).then(res => res.data)
   }
-};
+}
