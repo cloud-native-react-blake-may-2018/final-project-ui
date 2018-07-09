ea92790e4725aa83c18df9314234a00bdaffcd68
TylerPierro <i.tyler.pierro@gmail.com>
2018-07-09 12:54:50 -0400

    attempts to update on code to token

--- client/app/components/SplashPage.tsx
+++ client/app/components/SplashPage.tsx
@@ -1,5 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Axios from 'axios';
+
+/** Get the code from the social signin */
+const uri = document.location.href;
+// console.log(uri);
+const index = uri.indexOf('code=') + 5;
+const AUTHORIZATION_CODE = uri.slice(index,uri.length);
+// console.log(code);
+
+Axios.post("https://quizard.auth.us-east-2.amazoncognito.com/oauth2/token", {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic aSdxd892iujendek328uedj',
+
+    'grant_type': 'authorization_code',
+    'client_id': '1q83lmu6khfnc0v8jjdrde9291',
+    'code': AUTHORIZATION_CODE,
+    'redirect_uri': 'localhost:3222/dashboard'
+    }
+  })
+  .then(resp => {
+    localStorage.setItem('access_token', resp.data.access_token);
+    localStorage.setItem('id_token', resp.data.id_token);
+    localStorage.setItem('type', resp.data.type);
+    localStorage.setItem('expire', resp.data.expire);
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
 export const SplashPage = () => (
   <div>
