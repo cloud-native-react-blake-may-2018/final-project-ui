4d96a6c4cdb5c7b3a3b3a3f95075f10263d30ffd
TylerPierro <i.tyler.pierro@gmail.com>
2018-07-09 15:58:42 -0400

    Got the token!  Saved in localStorage

--- client/app/components/SplashPage.tsx
+++ client/app/components/SplashPage.tsx
@@ -1,30 +1,58 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import Axios from 'axios';
+import Querystring from 'query-string';
 
 /** Get the code from the social signin */
 const uri = document.location.href;
 // console.log(uri);
-const index = uri.indexOf('code=') + 5;
-const AUTHORIZATION_CODE = uri.slice(index,uri.length);
-// console.log(code);
+/** Start after access_token= */
+const startIndex = uri.indexOf('access_token=') + 13;
+/** End on & before id_token= */
+const endIndex = uri.indexOf('id_token=') - 1;
+const access_token = uri.slice(startIndex, endIndex);
+console.log(access_token);
 
-Axios.post("https://quizard.auth.us-east-2.amazoncognito.com/oauth2/token", {
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'Basic aSdxd892iujendek328uedj',
+// Axios.get("https://quizard.auth.us-east-1.amazoncognito.com/oauth2/authorize?response_type=code&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=https://localhost:3222/dashboard")
+//   .then(resp => {
+//     localStorage.setItem('location', resp.data.Location);
+//   })
 
-    'grant_type': 'authorization_code',
-    'client_id': '1q83lmu6khfnc0v8jjdrde9291',
-    'code': AUTHORIZATION_CODE,
-    'redirect_uri': 'localhost:3222/dashboard'
-    }
-  })
+// const postData = Querystring.stringify({
+//   'grant_type': 'authorization_code',
+//   'client_id': '1q83lmu6khfnc0v8jjdrde9291',
+//   'code': AUTHORIZATION_CODE,
+//   // 'redirect_uri': 'https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=code&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=http://localhost:3222/',
+//   'redirect_uri': 'localhost:3222/dashboard'
+// })
+// const axiosConfig = {
+//   'headers': {
+//     'Content-Type': 'application/x-www-form-urlencoded',
+//     // 'Authorization': 'Basic MXE4M2xtdTZraGZuYzB2OGpqZHJkZTkyOTE='
+//   }
+// }
+
+// Axios.post("https://quizard.auth.us-east-2.amazoncognito.com/oauth2/token", postData, axiosConfig)
+//   .then(resp => {
+//     localStorage.setItem('access_token', resp.data.access_token);
+//     localStorage.setItem('id_token', resp.data.id_token);
+//     localStorage.setItem('type', resp.data.type);
+//     localStorage.setItem('expire', resp.data.expire);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   })
+
+const axiosConfig = {
+  'headers': {
+    'Authorization': `Bearer ${access_token}`
+  }
+}
+Axios.get('https://quizard.auth.us-east-2.amazoncognito.com/oauth2/userInfo', axiosConfig)
   .then(resp => {
-    localStorage.setItem('access_token', resp.data.access_token);
-    localStorage.setItem('id_token', resp.data.id_token);
-    localStorage.setItem('type', resp.data.type);
-    localStorage.setItem('expire', resp.data.expire);
+    const stringResp = JSON.stringify(resp.data);
+    localStorage.setItem('token', stringResp);
+    localStorage.setItem('name', JSON.stringify(resp.data.name));
   })
   .catch(err => {
     console.log(err);
