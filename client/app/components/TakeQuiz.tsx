581e2f8a45a8035455e7a1ac38a01fc2d2faf866
Fernando Hudson <fernando_hudson@redlands.edu>
2018-07-10 10:38:21 -0400

    previous and next question is now stored in the redux store

--- client/app/components/TakeQuiz.tsx
+++ client/app/components/TakeQuiz.tsx
@@ -10,7 +10,6 @@ interface IProps {
   username: any
   quiz: any
   answerArray: object[]
-  questionNumber: number
   setQuestionNumber: (questionNumber: number) => void
   changeQuestionNumber: (questionNumber: number) => void
   startAddAnswerToArray: (answers: {}) => any
@@ -20,7 +19,7 @@ const questionStyle = {
   cursor: 'poninter'
 }
 
-export class TakeQuizPage extends Component<IProps> {
+export class TakeQuizPage extends Component<any, any> {
   constructor(props) {
     super(props)
   }
@@ -44,53 +43,36 @@ export class TakeQuizPage extends Component<IProps> {
 
   public previousQuizQuestion = (e: any) => {
     e.preventDefault()
-    this.setState({
-      ...this.state,
-      questionNumber: (this.state.questionNumber -= 1)
-    })
+    this.props.changeQuestionNumber(this.props.questionNumber - 1)
   }
   public nextQuizQuestion = (choices: object) => {
-    console.log(choices[this.state.questionNumber].format)
+    console.log(choices[this.props.questionNumber].format)
     if (
-      choices[this.state.questionNumber].format === 'multiple-choice' ||
-      choices[this.state.questionNumber].format === 'true-false'
+      choices[this.props.questionNumber].format === 'multiple-choice' ||
+      choices[this.props.questionNumber].format === 'true-false'
     ) {
-      // this.props.startAddAnswerToArray(
-      //   this.state.answerTrueFalseOrMultipleChoice
-      // )
-      this.setState({
-        ...this.state,
-        questionNumber: this.state.questionNumber + 1
-      })
-      // this.props.changeQuestionNumber(this.props.questionNumber + 1)
+      this.props.startAddAnswerToArray(
+        this.state.answerTrueFalseOrMultipleChoice
+      )
+      this.props.changeQuestionNumber(this.props.questionNumber + 1)
       console.log(this.props.questionNumber)
-    } else {
-      // this.props.startAddAnswerToArray(this.state.answerMultipleSelect)
+    } else if (
+      choices[this.props.questionNumber].format === 'multiple-select'
+    ) {
+      this.props.startAddAnswerToArray(this.state.answerMultipleSelect)
       console.log(this.props.questionNumber)
-      // this.props.changeQuestionNumber(this.props.questionNumber + 1)
-      this.setState({
-        ...this.state,
-        questionNumber: this.state.questionNumber + 1
-      })
+      this.props.changeQuestionNumber(this.props.questionNumber + 1)
     }
   }
 
   public addAnswerToObject = (choices: object, answer: any) => {
-    // SPECIFIC ANSWER THAT USER HAS SELECTED
-    console.log(answer.answer)
-    // FORMAT OF THE QUESTION
-    console.log(choices[this.state.questionNumber].title)
-    console.log(choices[this.state.questionNumber].author)
-    console.log(choices[this.state.questionNumber].format)
-    // console.log(choices[this.state.questionNumber].answers)
-
     switch (choices[this.state.questionNumber].format) {
       case 'multiple-choice':
         this.setState({
           ...this.state,
           answerTrueFalseOrMultipleChoice: {
-            author: choices[this.state.questionNumber].author,
-            title: choices[this.state.questionNumber].title,
+            author: choices[this.props.questionNumber].author,
+            title: choices[this.props.questionNumber].title,
             answer: answer.answer
           }
         })
@@ -105,8 +87,8 @@ export class TakeQuizPage extends Component<IProps> {
           this.setState({
             ...this.state,
             answerMultipleSelect: {
-              author: choices[this.state.questionNumber].author,
-              title: choices[this.state.questionNumber].title,
+              author: choices[this.props.questionNumber].author,
+              title: choices[this.props.questionNumber].title,
               answer: this.state.answerMultipleSelect.answer
             }
           })
@@ -114,26 +96,25 @@ export class TakeQuizPage extends Component<IProps> {
           this.setState({
             ...this.state,
             answerMultipleSelect: {
-              author: choices[this.state.questionNumber].author,
-              title: choices[this.state.questionNumber].title,
+              author: choices[this.props.questionNumber].author,
+              title: choices[this.props.questionNumber].title,
               answer: [...this.state.answerMultipleSelect.answer, answer.answer]
             }
           })
         }
-
-        console.log(this.state.answerMultipleSelect)
+        // console.log(this.state.answerMultipleSelect)
         break
 
       case 'true-false':
         this.setState({
           ...this.state,
           answerTrueFalseOrMultipleChoice: {
-            author: choices[this.state.questionNumber].author,
-            title: choices[this.state.questionNumber].title,
+            author: choices[this.props.questionNumber].author,
+            title: choices[this.props.questionNumber].title,
             answer: answer.answer
           }
         })
-        console.log(this.state.answerTrueFalseOrMultipleChoice)
+        // console.log(this.state.answerTrueFalseOrMultipleChoice)
         break
     }
   }
@@ -144,6 +125,7 @@ export class TakeQuizPage extends Component<IProps> {
 
   render() {
     const { quiz } = this.props
+    console.log('in render' + this.props.questionNumber)
     return (
       <div className="viewQuizzes-page">
         {quiz.questions === undefined && (
@@ -154,17 +136,17 @@ export class TakeQuizPage extends Component<IProps> {
             {/* DISPLAYS QUIZ TITLE */}
             <h1>{quiz.title}</h1>
             {/* DISPLAYS WHAT QUESTION NUMBER THE USER IS ON */}
-            <h3>Question {this.state.questionNumber + 1}</h3>
+            <h3>Question {this.props.questionNumber + 1}</h3>
             {/* DISPLAYS THE QUESTION */}
-            <h5>{quiz.questions[this.state.questionNumber].title}</h5>
+            <h5>{quiz.questions[this.props.questionNumber].title}</h5>
             {/* DISPLAYS THE QUESTION NUMBER THEY ARE ON OUT OF TOTAL */}
             <h3>
-              Question {this.state.questionNumber + 1}/{quiz.questions.length}
+              Question {this.props.questionNumber + 1}/{quiz.questions.length}
             </h3>
             {/* DISPLAYS THE CHOICES */}
             <div>
               {/* {console.log(quiz.questions)} */}
-              {quiz.questions[this.state.questionNumber].answers.map(
+              {quiz.questions[this.props.questionNumber].answers.map(
                 answers => (
                   <div style={questionStyle} key={answers.answer}>
                     <p
@@ -181,10 +163,10 @@ export class TakeQuizPage extends Component<IProps> {
               )}
             </div>
 
-            {this.state.questionNumber !== 0 ? (
+            {this.props.questionNumber !== 0 ? (
               <button onClick={this.previousQuizQuestion}>previous</button>
             ) : null}
-            {this.state.questionNumber + 1 !== quiz.questions.length ? (
+            {this.props.questionNumber + 1 !== quiz.questions.length ? (
               <button
                 onClick={this.nextQuizQuestion.bind(this, quiz.questions)}
               >
@@ -205,7 +187,8 @@ const mapStateToProps = (state, props) => ({
   quiz:
     state.quizzes.all !== undefined &&
     state.quizzes.all.find(quiz => quiz.uuid === props.match.params.uuid),
-  answers: state.quizzes.answers
+  answers: state.quizzes.answers,
+  questionNumber: state.takeQuiz.questionNumber
   // clickedQuestion: state.quizzes.clickedQuestion
 })
 
