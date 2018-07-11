c597a0a364dcdc40795c244119683bf2d24307f2
Fernando Hudson <fernando_hudson@redlands.edu>
2018-07-11 10:46:31 -0400

    stuff

--- client/app/components/SignInRedirect.tsx
+++ client/app/components/SignInRedirect.tsx
@@ -6,18 +6,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { startCreateNewQuiz } from '../actions/create'
 
 export class SignInRedirect extends Component {
-    componentDidMount () {
-        let redirectUrl = 'https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=http://localhost:3222/dashboard'
-        window.location.href = redirectUrl
-    }
+  componentDidMount() {
+    let redirectUrl =
+      'https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=http://localhost:3222/dashboard/'
+    window.location.href = redirectUrl
+  }
 
   render() {
-    
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div />
   }
 }
 
