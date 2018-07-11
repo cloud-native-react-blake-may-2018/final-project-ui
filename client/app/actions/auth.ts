aa5328446e64f5da28f624b2b5d1802b54d1b16f
Darius <dpugh3@aum.edu>
2018-07-11 10:14:40 -0400

    login fixed

--- client/app/actions/auth.ts
+++ client/app/actions/auth.ts
@@ -9,8 +9,9 @@ export const login = user => ({
 
 export const startLogin = credentials => {
   return async dispatch => {
+    console.log('credentials', credentials)
     await dispatch(login(credentials))
-    localStorage.setItem('token', credentials.token)
+    // localStorage.setItem('token', credentials.token)
 
     return await dispatch(startGetUserQuizzes(credentials.username))
   }
