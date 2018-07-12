479505626d04f60ab704367e3871c6f8cc2e1bd7
Scott <sm.scottkmoore@gmail.com>
2018-07-12 15:21:20 -0400

    Changed redirect URL to agree with Cloudfront

--- client/app/components/SignInRedirect.tsx
+++ client/app/components/SignInRedirect.tsx
@@ -12,12 +12,12 @@ import { environment, prod, dev } from '../environment/config'
 //   context: "https://8lomsjt0a6.execute-api.us-west-2.amazonaws.com/prod/"
 //  };export const environment = process.env.NODE_ENV === "production" ? prod : dev;
 
-let callbackUrl = environment === prod ? 'https://cloud-native-project-3-ui.s3-website.us-east-2.amazonaws.com/redirect' : 'http://localhost:3222/redirect'
+let callbackUrl = environment === prod ? 'https://dwea2klqp52vb.cloudfront.net/index.html' : 'http://localhost:3222/redirect'
 
 export class SignInRedirect extends Component {
   componentDidMount() {
     let redirectUrl =
-      'https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=http://localhost:3222/redirect'
+      'https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=' + callbackUrl
     window.location.href = redirectUrl
   }
 
