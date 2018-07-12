89e82ecfdc55596da3a94872c2507014cfb06c23
Eric Morrison <malin1@protonmail.ch>
2018-07-12 14:00:23 -0400

    dynamic signin url

--- client/app/components/SignInRedirect.tsx
+++ client/app/components/SignInRedirect.tsx
@@ -8,15 +8,16 @@ import { environment } from '../../../environment'
 
 export class SignInRedirect extends Component {
   componentDidMount() {
-    //   let redirectUrl = `https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=${
-    //     environment.context
-    //   }/redirect`
-    // }
-    console.log('env: ', environment.context)
-    let redirectUrl =
-      'https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=http://localhost:3222/redirect'
-    window.location.href = redirectUrl
+    console.log('environment: ', environment.context)
+    let redirectUrl = `https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=${
+      environment.context
+    }/redirect`
   }
+  //   console.log('env: ', environment.context)
+  //   let redirectUrl =
+  //     'https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=http://localhost:3222/redirect'
+  //   window.location.href = redirectUrl
+  // }
 
   render() {
     return <div />
