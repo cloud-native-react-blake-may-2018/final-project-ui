3acb925fc5cc1c1666277aa2cb2c044e05404e04
Eric Morrison <malin1@protonmail.ch>
2018-07-12 15:52:16 -0400

    fixed issues related to development and production environments

--- client/app/components/SignInRedirect.tsx
+++ client/app/components/SignInRedirect.tsx
@@ -8,17 +8,11 @@ import { environment } from '../../../environment'
 
 export class SignInRedirect extends Component {
   componentDidMount() {
-    console.log('environment: ', environment.context)
     let redirectUrl = `https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=${
       environment.context
     }/redirect`
     window.location.href = redirectUrl
   }
-  //   console.log('env: ', environment.context)
-  //   let redirectUrl =
-  //     'https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=http://localhost:3222/redirect'
-  //   window.location.href = redirectUrl
-  // }
 
   render() {
     return <div />
--- environment.ts
+++ environment.ts
@@ -2,7 +2,8 @@ const dev = {
   context: `http://localhost:${process.env.NODE_PORT}`
 }
 const prod = {
-  context: 'http://cloud-native-project-3-ui.s3-website.us-east-2.amazonaws.com'
+  context: 'https://dwea2klqp52vb.cloudfront.net'
+  // context: 'http://cloud-native-project-3-ui.s3-website.us-east-2.amazonaws.com'
 }
 
 export const environment = process.env.NODE_ENV === 'production' ? prod : dev
