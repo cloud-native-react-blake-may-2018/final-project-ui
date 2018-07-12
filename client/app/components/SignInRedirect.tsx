464e2f549c53415692b54c29ae6727d690c7e9e8
Eric Morrison <malin1@protonmail.ch>
2018-07-12 14:05:01 -0400

    added window.location.href

--- client/app/components/SignInRedirect.tsx
+++ client/app/components/SignInRedirect.tsx
@@ -12,6 +12,7 @@ export class SignInRedirect extends Component {
     let redirectUrl = `https://quizard.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=1q83lmu6khfnc0v8jjdrde9291&redirect_uri=${
       environment.context
     }/redirect`
+    window.location.href = redirectUrl
   }
   //   console.log('env: ', environment.context)
   //   let redirectUrl =
