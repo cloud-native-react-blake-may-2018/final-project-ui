1e058a845861fd2606dd29c0fdbf472525020d96
Eric Morrison <malin1@protonmail.ch>
2018-07-12 16:52:34 -0400

    working

--- environment.ts
+++ environment.ts
@@ -5,4 +5,9 @@ const prod = {
   context: 'https://dwea2klqp52vb.cloudfront.net'
 }
 
+// possible solution to invalidating the cache; add a query parameter
+// const prod = {
+//   context: 'https://dwea2klqp52vb.cloudfront.net?v=2'
+// }
+
 export const environment = process.env.NODE_ENV === 'production' ? prod : dev
