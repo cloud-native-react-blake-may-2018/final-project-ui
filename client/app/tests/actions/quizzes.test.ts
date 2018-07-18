d7b14298990806a09e909d8f4d198bfb398bbca0
Eric Morrison <malin1@protonmail.ch>
2018-07-17 22:11:51 -0400

    annotated action test

--- client/app/tests/actions/quizzes.test.ts
+++ client/app/tests/actions/quizzes.test.ts
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import { editQuiz } from '../../actions/quizzes'
 
-test('should test search action', () => {
+test('should test editQuiz action', () => {
+
+  // setup data to pass to action
   const quiz = {
     quizID: '2341241242124124',
     question: 'quiz-question'
   }
+
+  // call action with data
   const action = editQuiz(quiz)
 
+  // expect action to include type
   expect(action).toEqual({
     type: 'EDIT_QUIZ',
     quiz
